914666dd9bc2c02cdfc4d196561e842b31d8e591
philippvlasov <pylyp.vlasov@valtech.com>
2020-05-25 20:29:08 +0300

    header with main section slider

---
+++ src/js/index.js
@@ -0,0 +1,40 @@
+(function ($) {
+  const Component = function (container) {
+    this.$container = $(container);
+    this.imageSlide = this.$container.find('.slider-image');
+  };
+
+  $.extend(true, Component.prototype, {
+    init() {
+      this.initializeSlider();
+      this.setImageToBakcground();
+      this.$container.addClass('initialized');
+    },
+
+    setImageToBakcground() {
+      this.imageSlide.each((index, item) => {
+        console.log($(item).find('img')[0].src);
+        const imagePath = $(item).find('img')[0].src;
+        $(item).closest('.slick-slide').css('background-image', `url("${imagePath}")`)
+      })
+    },
+
+    initializeSlider() {
+      this.$container.slick({
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+      });
+
+    },
+  });
+  const $component = $('.slider:not(.initialized)');
+  let instance;
+
+  $component.each((i, item) => {
+    instance = new Component(item);
+    instance.init();
+  });
+}(jQuery));
