ec2bd01386f69a7203ae2b20ec0562f617e1e9e1
philippvlasov <pylyp.vlasov@valtech.com>
2020-05-27 19:17:14 +0300

    Video slider added

--- src/js/index.js
+++ src/js/index.js
@@ -2,37 +2,53 @@
   const Component = function (container) {
     this.$container = $(container);
     this.imageSlide = this.$container.find('.slider-image');
+    this.slideToShow = 1;
   };
 
   $.extend(true, Component.prototype, {
     init() {
+      this.configureVideSlider();
       this.initializeSlider();
       this.checkSliderType();
       this.$container.addClass('initialized');
     },
 
     checkSliderType() {
-      console.log(this.$container.hasClass('mixed-content'));
       if (this.$container.hasClass('mixed-content')) {
         this.setImageToBakcground();
       }
     },
 
+    configureVideSlider() {
+      if (this.$container.hasClass('video-slider')) {
+        this.slideToShow = 2;
+      }
+    },
+
     setImageToBakcground() {
       this.imageSlide.each((index, item) => {
-        console.log($(item).find('img')[0].src);
         const imagePath = $(item).find('img')[0].src;
         $(item).closest('.slick-slide').css('background-image', `url("${imagePath}")`)
       })
     },
 
     initializeSlider() {
+      // console.log(this.slideToShow);
       this.$container.slick({
         infinite: false,
-        slidesToShow: 1,
+        slidesToShow: this.slideToShow,
         slidesToScroll: 1,
         arrows: false,
-        dots: true,
+        dots: false,
+        responsive: [
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 1,
+              dots: true
+            }
+          }
+        ]
       });
 
     },
@@ -45,3 +61,10 @@
     instance.init();
   });
 }(jQuery));
+
+const $showButton = $('.show-btn');
+
+$('.show-btn').on('click', function () {
+  $('.hidden-text').slideDown();
+  $('.show-btn').hide();
+})
